86397d1fbec4ddf4797ce9a15900f527f07cd860
Larissa-Kailane <larissakailanelv@gmail.com>
2025-03-11 11:20:08 -0300

    Adicionando matriz (vetor de vetores).

--- array_funcao_objeto.js
+++ array_funcao_objeto.js
@@ -30,3 +30,45 @@ imprimirResultado(num_A, num_B, operacoes[3], num_C);
 num_C = num_A + num_B;
 console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${num_C}`);
 */
+
+//Matriz
+//vetor de vetores
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+//o índice é o número da linha e da coluna
+
+function imprimirMatriz(A){
+    for(let i = 0; i < A.length; i++){
+        for(let j = 0; j < A[i].length; j++){
+            linha += A[i][j] + "\t";
+        }
+        console.log(linha);
+    }
+}
+imprimirMatriz(matriz);
+
+let capitatis = {
+    DF: "Brasília",
+    DDD: 61,
+    SP: "São Paulo",
+    DDD: 11,
+    RJ: "Rio de Janeiro",
+    DDD: 21
+}
+
+for(chave in capitais){
+    console.log(chave + "=" + capitais[chave]);
+}
+
+function verificarParidade(n){
+    if(n % 2 == 0){
+        return true;
+    }
+    return false;
+}
+
+console.log(`Verificar paridade(${n}=${verificarParidade(n)})`);
+n = 3;
