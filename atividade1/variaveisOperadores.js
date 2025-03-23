3178848ad5d503b387d90736046e5aad1910573b
Larissa-Kailane <larissakailanelv@gmail.com>
2025-03-22 23:29:21 -0300

    Subindo a correção da atividade 1.

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,3 +1,26 @@
+//Variáveis
+let num_A = 2;
+let num_B = 3.0;
+let num_C;
+
+//soma
+num_C = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B} é ${num_C}`);
+
+//subtração
+num_C = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${num_C}`);
+
+//multiplicação
+num_C = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${num_C}`);
+
+//divisão
+num_C = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${num_C}`);
+
+
+/*
 //variáveis
 let num_A = 6;
 let num_B = 3;
@@ -21,5 +44,5 @@ console.log(minhaString);
 resultado = 6 - 3;
 minhaString = `Resultado da subtração entre ${num_A} e ${num_B} é ${resultado}`;
 console.log(minhaString);
-
+*/
  
