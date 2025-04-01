a6de81fe1e02931d14cf2518ca7fdb0703c6e7d4
Larissa-Kailane <larissakailanelv@gmail.com>
2025-04-01 10:37:52 -0300

    =Subindo as correções e anotações da atividade 3.

--- atividade3/calculadora.js
+++ atividade3/calculadora.js
@@ -1,4 +1,10 @@
 //Funções de calculo
+/* 
+    Esse módulo implementa as operações básicas de uma calculadora, que devem estar disponíveis para
+    outras partes do código.
+*/
+
+
 function somar(a, b){
     return a + b;
 }
@@ -22,3 +28,37 @@ module.exports = {
     multiplicar,
     dividir
 }
+
+/*
+
+Outra forma é ultilizar a palavra export antes da função
+
+export function dividir(a, b){
+    return a / b;
+}
+
+*/
+
+
+
+//CORREÇÃO DA AULA - NOTAS
+/* Criar as funções
+- Mesmo código do professor^^
+
+export function somar(a, b){
+    return a + b;
+}
+
+export function subtrair(a, b){
+    return a - b;
+}
+
+export function multiplicar(a, b){
+    return a * b;
+}
+
+export function dividir(a, b){
+    return a / b;
+}
+
+*/
--- atividade3/index.js
+++ atividade3/index.js
@@ -1,16 +1,63 @@
-const calc = require('./calculadora');
+const calc = require('./calculadora'); //acesso as funções que o módulo disponibiliza
+//./util/calculadora
+
+//uma aplicação web é um processo que fica sendo executado em um sistema operacional
 
 //importando o express - constante para garantir que não muda
 const express = require('express');
 
-//aplicação web
+//aplicação web - cria uma apliacação do tipo web
 const app = express();
 
+//Criando as rotas
+
+app.get('/', (req, res) => { // req - objeto de requisição; res - objeto de resposta
+    let html = '<h1>app_calculadora</h1>';
+    html += '<h3>Rotas disponíveis</h3>';
+    html += '<p>/somar/:a/:b  <a href= "/somar/1/2">Testar</a></p>';
+    html += '<p>/subtrair/:a/:b  <a href= "/subtrair/1/2">Testar</a></p>';
+    html += '<p>/multiplicar/:a/:b  <a href= "/multiplicar/1/2">Testar</a></p>';
+    html += '<p>/dividir/:a/:b  <a href= "/dividir/1/2">Testar</a></p>';
+    res.send(html);
+});
+
+
 // Soma
-app.get('/somar/:a/:b', (req, res) => {
+app.get('/somar/:a/:b', (req, res) => { // req - objeto de requisição; res - objeto de resposta
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} + ${b} = ${calc.somar(a, b)}`);
+});
+
+// Subtrair
+app.get('/subtrair/:a/:b', (req, res) => { // req - objeto de requisição; res - objeto de resposta
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} - ${b} = ${calc.subtrair(a, b)}`);
+});
+
+// Multiplicar
+app.get('/multiplicar/:a/:b', (req, res) => { // req - objeto de requisição; res - objeto de resposta
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} * ${b} = ${calc.multiplicar(a, b)}`);
+});
+
+// Dividir
+app.get('/dividir/:a/:b', (req, res) => { // req - objeto de requisição; res - objeto de resposta
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} / ${b} = ${calc.dividir(a, b)}`);
+});
+
+
+/*
+parseFloat  - converte uma string para números de ponto flutuante
+// Soma
+app.get('/soma/:a/:b', (req, res) => {
     const a = parseFloat(req.params.a);
     const b = parseFloat(req.params.b);
-    res.send(`A SOMA é igual a: ${calc.somar(a, b)}`);
+    res.send(`A SOMA é igual a: ${calc.subtrair(a, b)}`);
 });
 
 // Subtração
@@ -37,10 +84,11 @@ app.get('/dividir/:a/:b', (req, res) => {
     }
     res.send(`A DIVISÃO é igual a: ${calc.dividir(a, b)}`);
 });
-
+*/
 
 //DECLARANDO UMA PORTA
+//localhost - nossa própria máquina
 const PORT = 8080;
-app.listen(PORT, ()=>{
+app.listen(PORT, ()=>{ //função anônima
     console.log('app rodando na porta ' + PORT);
 });
